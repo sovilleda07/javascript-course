c83039457cd19c43613b5451098ba18641fa9cef
sovilleda07 <sovilleda07@gmail.com>
2025-11-15 21:37:11 -0600

    Lesson 11

---
+++ 11-lesson/scripts/todo-list.js
@@ -0,0 +1,27 @@
+let todoList = ['Watch YouTube','Make dinner'];
+
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHTML = '';
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;
+    todoListHTML += html;
+  }
+
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
+
+
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+  
+  todoList.push(name);
+
+  inputElement.value = '';
+
+  renderTodoList();
+};

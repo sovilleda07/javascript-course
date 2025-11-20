5e036122e37f64e1e6bb5c0063175ce5e61f8563
sovilleda07 <sovilleda07@gmail.com>
2025-11-19 22:59:15 -0600

    Lesson 12 Exercises

---
+++ 12-lesson/scripts/todo-list.js
@@ -0,0 +1,47 @@
+let todoList = [{
+  name: 'Watch YouTube',
+  dueDate: '2025-12-22'
+},{
+  name: 'Make dinner',
+  dueDate: '2025-11-30'
+}];
+
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHTML = '';
+
+  todoList.forEach(function(todoObject, index) {
+    const { name, dueDate } = todoObject;
+
+    const html = `
+    <div>${name}</div>
+    <div>${dueDate}</div>
+    <button onClick="
+      todoList.splice(${index}, 1);
+      renderTodoList();
+    " class="delete-todo-button">Delete</button>
+    `;
+    todoListHTML += html;
+  });
+
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
+
+
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+
+  todoList.push({
+    name,
+    dueDate
+  });
+
+  inputElement.value = '';
+
+  renderTodoList();
+};

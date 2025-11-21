add5484cff3e26a3ea6a79f7cbe2188286de0f6e
sovilleda07 <sovilleda07@gmail.com>
2025-11-20 21:41:10 -0600

    Lesson 12 Part 2

--- 12-lesson/scripts/rock-paper-scissors.js
+++ 12-lesson/scripts/rock-paper-scissors.js
@@ -60,9 +60,12 @@ function playGame(playerMove) {
 let isAutoPlaying = false;
 let intervalId;
 
+// const autoPlay = () => {
+// };
+
 function autoPlay() {
   if (!isAutoPlaying) {
-     intervalId = setInterval(function() {
+     intervalId = setInterval(() => {
       const playerMove = pickComputerMove();
       playGame(playerMove);
     }, 1000);
@@ -73,6 +76,28 @@ function autoPlay() {
     isAutoPlaying = false;
   }
 }
+
+document.querySelector('.js-rock-button').addEventListener('click', () => {
+  playGame('rock');
+});
+
+document.querySelector('.js-paper-button').addEventListener('click', () => {
+  playGame('paper');
+});
+
+document.querySelector('.js-scissors-button').addEventListener('click', () => {
+  playGame('scissors');
+});
+
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === 'r') {
+    playGame('rock');
+  } else if (event.key === 'p') {
+    playGame('paper');
+  } else if (event.key === 's') {
+    playGame('scissors');
+  }
+})
 
 function updateScoreElement() {
   document.querySelector('.js-score')
